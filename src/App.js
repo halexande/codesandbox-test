c2ed6172f39f63ed8a0447d6447aeda43e982be4
halexande <35641589+halexande@users.noreply.github.com>
2020-04-17 18:54:52 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>ChessRoom</h1>
+      <h2>This is the Chess Room</h2>
+      <p>More text</p>
+    </div>
+  );
+}
